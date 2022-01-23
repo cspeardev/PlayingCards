80f4dc639c9f226464ebff4768dc804d7c0ef882
Connor Spear <cspear1994@gmail.com>
2022-01-23 18:14:48 -0500

    Add base functionality

---
+++ Cards/CardsLibrary/Card.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Cards
+{
+    public class Card
+    {
+        public Card(suit? Suit, rank Rank)
+        {
+            CardSuit = Suit;
+            CardRank = Rank;
+        }
+        public enum suit
+        {
+            hearts,
+            spades,
+            diamonds,
+            clubs
+        }
+        public enum color
+        {
+            red,
+            black,
+            blue
+        }
+        public enum rank
+        {
+            ace = 'A',
+            two = 2,
+            three = 3,
+            four = 4,
+            five = 5,
+            six = 6,
+            seven = 7,
+            eight = 8,
+            nine = 9,
+            ten = 10,
+            jack = 'J',
+            queen = 'Q',
+            king = 'K',
+            joker
+        }
+        public color CardColor { get; }
+        public rank CardRank { get; }
+        public suit? CardSuit { get; }
+
+        public string SVG { get; }
+        public bool IsFaceCard { get
+            {
+                if (CardRank.GetType() == typeof(int) || CardRank == rank.joker)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            } 
+        }
+        public override string ToString()
+        {
+            if(CardRank == Card.rank.joker)
+            {
+                return "Joker";
+            }
+            else
+            {
+                return String.Format("{0} of {1}", CardRank, CardSuit);
+            }
+        }
+
+
+    }
+}
---
+++ Cards/CardsLibrary/Deck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards
+{
+    public class Deck
+    {
+        public enum format
+        {
+            Italian
+        }
+        private List<Card> cards;
+
+        public List<Card> Cards { get => cards; set => cards = value; }
+
+        public Deck(format DeckFormat = format.Italian, int Jokers = 0)
+        {
+            Cards = new List<Card>();
+            List<Card.suit> DeckSuits = null;
+            List<Card.rank> DeckRanks = null;
+
+            switch (DeckFormat)
+            {
+                case format.Italian:
+                    DeckSuits = new List<Card.suit> { Card.suit.clubs, Card.suit.hearts, Card.suit.spades, Card.suit.diamonds };
+                    DeckRanks = new List<Card.rank> { Card.rank.ace, Card.rank.two, Card.rank.three, Card.rank.four, Card.rank.five, Card.rank.six, Card.rank.seven, Card.rank.eight, Card.rank.nine, Card.rank.ten, Card.rank.jack, Card.rank.queen, Card.rank.king };
+                    break;
+
+            }
+
+
+            foreach (Card.suit s in DeckSuits)
+            {
+                foreach (Card.rank r in DeckRanks)
+                {
+                    Cards.Add(new Card(s, r));
+                }
+            }
+
+            for (int i = 0;i < Jokers; i++) cards.Add(new Card(null, Card.rank.joker));
+        }
+    }
+}

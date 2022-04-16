e761735d311841530420f7eb2516daff9bcc230c
Connor Spear <cspear1994@gmail.com>
2022-04-16 16:08:51 -0400

    Strip down to French suits until functionality is completed.

--- Cards/CardsLibrary/Suit.cs
+++ Cards/CardsLibrary/Suit.cs
@@ -6,16 +6,5 @@ public enum Suit
     hearts = 9829,
     spades = 9824,
     diamonds = 9830,
-    clubs = 9827,
-    //German suited
-    bells,
-    acorns,
-    leaves,
-    //Swiss suited
-    shields,
-    roses,
-    //Spanish suited
-    coins,
-    cups
-
+    clubs = 9827
 }
